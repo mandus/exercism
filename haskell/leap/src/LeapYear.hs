260dfd4046eb8c38f85d7fed469a53b81079c085
Åsmund Ødegård <mandus@gmail.com>
2019-02-24 16:38:16 +0100

    Improve leap year based on review

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,8 +1,12 @@
 module LeapYear (isLeapYear) where
 
+isRemYearZero :: Integer -> Integer -> Bool
+isRemYearZero year n = year `rem` n == 0 
+
 isLeapYear :: Integer -> Bool
 isLeapYear year 
-    | mod year 400 == 0 = True
-    | mod year 100 == 0 = False
-    | mod year 4 == 0 = True
+    | isRemNZero 400 = True
+    | isRemNZero 100 = False
+    | isRemNZero 4 = True
     | otherwise = False
+  where isRemNZero = isRemYearZero year
